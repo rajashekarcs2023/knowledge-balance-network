fc156e2871eed85e9467d793b30f10652fac2911
rajashekarcs2023 <vrajashekar2022mscs@gmail.com>
2025-02-15 00:42:16 -0800

    commit all

---
+++ app/database/supabase_client.py
@@ -0,0 +1,11 @@
+# app/database/supabase_client.py
+from supabase import create_client
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+supabase = create_client(
+    os.getenv("SUPABASE_URL"),
+    os.getenv("SUPABASE_KEY")
+)
